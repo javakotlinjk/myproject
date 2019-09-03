5a634a74c01251b75271b48cf8c4683d04c8b178
資訊服務部-劉春秀 <janet_liu@worldvision.org.tw>
2019-09-03 18:19:31 +0800

    Initial commit

---
+++ src/main/kotlin/com/janet/hello.kt
@@ -0,0 +1,15 @@
+package com.janet
+
+fun main(args: Array<String>) {
+ //println("Hello kotlin")
+  //  Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human{
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
+
+
