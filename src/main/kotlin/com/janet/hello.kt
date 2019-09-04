5c34e23767046753827e7d98cf07fea0f2255292
janet_liu <janet_liu@worldvision.org.tw>
2019-09-04 15:51:14 +0800

    java and kotlin variable

--- src/main/kotlin/com/janet/hello.kt
+++ src/main/kotlin/com/janet/hello.kt
@@ -5,6 +5,11 @@ fun main(args: Array<String>) {
   //  Human().hello()
     val h = Human()
     h.hello()
+    val age : Int = 19 // value 不能改變
+    val age2 = 19
+    var weight = 55.5f // variable 未來可改變
+    var name : String
+    name = "janet"
 }
 class Human{
     fun hello() {
